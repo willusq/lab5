39e7ed016e19b790856519b674a305edac1b53de
William. Palmer <willus31@gmail.com>
2016-04-06 03:01:59 -0400

    Finished

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,15 +1,144 @@
 import java.awt.List;
 
 public class HashTable {
-	int[] primes = new int[50];
+	int[] primes = new int[100];
+	Node[] table;
+	int tableSize=0;
 	
-	public HashTable(){
-		for (int i=0;i<primes.length;++i)
-			primes[i]=0;
+	public HashTable(int size){
+		table=new Node[size];
+		for(int i=0;i<table.length;++i)
+			table[i]=new Node();
+		tableSize=size;
+		Primes p=new Primes();
+		p.genPrimes(primes);
 		
-		genPrimes();
 	}
-	public boolean checkPrime(int n){
+	
+	public void add(String in){
+		int hash = hash(in);
+		Node head=table[hash];
+		while(head.next!=null){
+			head=head.next;
+		}
+		head.next=new Node(in,1,null);
+	}
+	
+	public boolean contains(String in){
+		int hash = hash(in);
+		Node head=table[hash];
+		while(head.next!=null){
+			if(in.equals(head.key))
+				return true;
+			head=head.next;
+		}
+		if(in.equals(head.key))
+			return true;
+		return false;
+		
+		
+	}
+	public int getValue(String in){
+		int hash = hash(in);
+		Node head=table[hash];
+		while(head.next!=null){
+			if(in.equals(head.key))
+				return head.value;
+			head=head.next;
+		}
+		if(in.equals(head.key))
+			return head.value;
+		return 0;
+	}
+	public void setValue(String key, int val){
+		int hash = hash(key);
+		Node head=table[hash];
+		while(head.next!=null){
+			if(key.equals(head.key))
+				head.value=val;
+			head=head.next;
+		}
+		if(key.equals(head.key))
+			head.value=val;
+		
+	}
+	public void incValue(String key  ){
+		int hash = hash(key);
+		Node head=table[hash];
+		while(head.next!=null){
+			if(key.equals(head.key))
+				++head.value;
+			head=head.next;
+		}
+		if(key.equals(head.key))
+			++head.value;
+		
+	}
+	public void printall(){
+		for(int i=0;i<table.length;++i){
+			Node head=table[i];
+			while(head.next!=null){
+				if(head.key!=null)
+					printVal(head);
+				head=head.next;
+			}	
+			if(head.key!=null)
+				printVal(head);
+		}
+	}
+	
+	
+	private void printVal(Node n){
+		System.out.println(n.key + " was found a total of "+ n.value + " times" );
+	}
+	private int hash(String in){
+		int out=0;
+		for(int i=0;i<in.length();++i)
+			out+=in.charAt(i)*primes[i]*primes[i+1];
+		return out%tableSize;
+	}
+	
+	
+}
+
+
+
+
+
+
+class Node{
+	String key;
+	int value;
+	Node next;
+	public Node(){
+		this(null,0,null);
+	}
+	public Node(String k, int v, Node n){
+		next=n;
+		value=v;
+		key=k;
+	}
+}
+
+class Primes{
+	public static int[] genPrimes(int[] primes){
+		int lastPrime=2;
+		primes[0]=lastPrime;
+		for(int i=1;i<primes.length;++i){
+			boolean done=true;
+			
+			for(int j=lastPrime+1;done;++j){
+				if(checkPrime(j,primes)){
+					primes[i]=j;
+					lastPrime=j;
+					done=false;
+				}
+			}
+		}
+		return primes;
+	}
+	
+	public static boolean checkPrime(int n, int[] primes){
 		for(int i=0;i<primes.length;++i){
 			if(primes[i]==0)return true;
 			else{
@@ -22,38 +151,4 @@ public class HashTable {
 		}
 		return false;
 	}
-	public void genPrimes(){
-		int lastPrime=2;
-		primes[0]=lastPrime;
-		for(int i=1;i<primes.length;++i){
-			boolean done=true;
-			
-			for(int j=lastPrime+1;done;++j){
-				if(checkPrime(j)){
-					System.out.println(j);
-					primes[i]=j;
-					lastPrime=j;
-					done=false;
-				}
-			}
-		}
-	}
-
-	public int hash(String in){
-		int out=0;
-		for(int i=0;i<in.length();++i)
-			out+=in.charAt(i)*primes[i];
-		return out;
-	}
-}
-
-
-
-
-
-
-class Node{
-	String key;
-	int value;
-	Node next;
 }
--- src/Test.java
+++ src/Test.java
@@ -1,15 +1,52 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
 
 public class Test {
 
 	public static void main(String[] args) {
-		HashTable h=new HashTable();
-		System.out.println("Hash Value of " +"dog"+ " is " + h.hash("dog"));
-		System.out.println("Hash Value of " +"god"+ " is " + h.hash("god"));
-		System.out.println("Hash Value of " +"stop"+ " is " + h.hash("stop"));
-		System.out.println("Hash Value of " +"pots"+ " is " + h.hash("pots"));
-		System.out.println("Hash Value of " +"stop"+ " is " + h.hash("stop"));
-		System.out.println("Hash Value of " +"pots"+ " is " + h.hash("pots"));
-
+		HashTable dict=new HashTable(1000);
+		HashTable counter=new HashTable(111);
+		
+		try (BufferedReader br = new BufferedReader(new FileReader("word.txt"))) {
+		    String line;
+		    while ((line = br.readLine()) != null)
+				dict.add(line.toLowerCase());//Adds each word to the dictionary
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+		}
+		
+		try (BufferedReader br = new BufferedReader(new FileReader("Comp 2071 - 2016-1sp -- Project 5 - Hash Tables -- DMR -- 2015-07-14 01.txt"))) {
+		    String line;
+		    while ((line = br.readLine()) != null) {
+		    	String[] currWords = line.split(" ");
+		    	for(int i=0;i<currWords.length;++i){
+		    		String curr=currWords[i].toLowerCase();
+		    		curr=curr.trim();
+		    		curr=curr.replaceAll("[^a-zA-Z ]", "");
+			    	if(dict.contains(curr)){//the word is found in the dictionary, and it is time to increment the counter or add it to the counter hashtable
+			    		if(counter.contains(curr))
+			    			counter.incValue(curr);
+			    			//counter.setValue(curr,counter.getValue(curr)+1);
+			    		else
+			    			counter.add(curr);
+				    	
+			    	}else{
+			    		if(curr.equals(""))
+			    			;
+			    		else
+			    			;//System.out.println(curr + " Was not found in the dictionary");		
+			    	}
+			    	
+		    
+		    	}
+		    }
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+		}
+		counter.printall();
 	}
-
 }
+
